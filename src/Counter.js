32b294e3ea95d32c809ba39018f925ae2e010ac9
youtangai <youtanagai@gmail.com>
2019-06-03 17:15:14 +0900

    完成

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-export default class Counter extends React.Component {
+export default class Counter extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0,
+        };
+
+        this.plus = this.plus.bind(this)
+        this.minus = this.minus.bind(this)
+    }
+
+    plus() {
+        this.setState({
+            number: this.state.number+1
+        })
+    }
+
+    minus() {
+        this.setState({
+            number: this.state.number-1
+        })
+    }
+
     render() {
-        return (
-            <div>this is counter app</div>
+        return ( 
+            <div>
+                <button onClick={this.minus}>マイナス</button>
+                {this.state.number}
+                <button onClick={this.plus}>プラス</button>
+            </div>
         )
     }
 }
