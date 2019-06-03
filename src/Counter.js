4a7392ac69e451ce786f2c5d54c056b43b810019
youtangai <youtanagai@gmail.com>
2019-06-03 16:40:47 +0900

    add Conterコンポーネントを追加

---
+++ src/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Counter extends React.Component {
+    render() {
+        return (
+            <div>this is counter app</div>
+        )
+    }
+}
